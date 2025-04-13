13a92bc481e30d141628a45ee039a6d626c168b3
isaihb <isaiahtechboy@gmail.com>
2025-04-12 21:00:09 -0500

    Added the html elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        
+        
       </header>
+      <button className = "add" id = "operation">+</button>
+      <button className = "subtract" id = "operation">-</button>
+      <button className = "multiply" id = "operation">*</button>
+      <button className = "divide" id = "operation">/</button>
+      <button className = "enter">=</button>
+      <button classname = "0" id = "num">0</button>
+      <button className = "1"  id = "num">1</button>
+      <button className = "2"  id = "num">2</button>
+      <button className = "3"  id = "num">3</button>
+      <button className = "4"  id = "num">4</button>
+      <button className = "5"  id = "num">5</button>
+      <button className = "6"  id = "num">6</button>
+      <button className = "7"  id = "num">7</button>
+      <button className = "8"  id = "num">8</button>
+      <button className = "9"  id = "num">9</button>
+      <button className = "AC"  id = "num">Clear</button>
+      <div className = "output">hi</div>
     </div>
+  
+    
   );
 }
 

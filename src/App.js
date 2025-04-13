66b84ceee244f80af2452e9982f450e5b6ccbeba
isaihb <isaiahtechboy@gmail.com>
2025-04-13 15:27:27 -0500

    Finished styling calculator

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
       <button className = "zero" id = "num">0</button>
       <button className = "one"  id = "num">1</button>
       <button className = "two"  id = "num">2</button>
-      <button className = "3"  id = "num">3</button>
-      <button className = "4"  id = "num">4</button>
-      <button className = "5"  id = "num">5</button>
-      <button className = "6"  id = "num">6</button>
-      <button className = "7"  id = "num">7</button>
-      <button className = "8"  id = "num">8</button>
-      <button className = "9"  id = "num">9</button>
+      <button className = "three"  id = "num">3</button>
+      <button className = "four"  id = "num">4</button>
+      <button className = "five"  id = "num">5</button>
+      <button className = "six"  id = "num">6</button>
+      <button className = "seven"  id = "num">7</button>
+      <button className = "eight"  id = "num">8</button>
+      <button className = "nine"  id = "num">9</button>
       <button className = "decimal" id = "num">.</button>
       <button className = "AC">Clear</button>
       <div className = "output">hi</div>
